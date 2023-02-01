b3ba10253c62142e1fed094f476f43cefff60fb1
student <sample@bths.edu>
2023-02-01 12:03:55 -0500

    Done

--- src/UsedCarLot.java
+++ src/UsedCarLot.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
 public class UsedCarLot {
-    private Car inventory:
+    private ArrayList<Car> inventory;
+
+    public UsedCarLot(){
+        inventory = new ArrayList<Car>();
+    }
+
+    public ArrayList<Car> getInventory(){
+        return inventory;
+    }
+
+    public void addCar(Car carToAdd){
+        inventory.add(carToAdd);
+    }
+    public boolean swap(int swap1, int swap2){
+        if(swap1<inventory.size() && swap2<inventory.size()){
+            Car car1 = inventory.get(swap1);
+            Car car2 = inventory.get(swap2);
+            inventory.set(swap1,car2);
+            inventory.set(swap2,car1);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
